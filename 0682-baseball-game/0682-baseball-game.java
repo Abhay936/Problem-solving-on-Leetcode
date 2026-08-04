c4b971510247c773f116d507886504f2faffd553
Abhay936 <sa5393359@gmail.com>
2026-08-04 12:05:49 +0530

    Time: 13 ms (3.34%), Space: 47.1 MB (5.66%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stk = new Stack<>();
+        int list[] = new int[operations.length];
+        for(String str : operations){
+            if(str.matches("-?\\d+")){
+                stk.push(Integer.parseInt(str));
+            }
+            if(str.equals("C")){
+                stk.pop();
+            }
+            if(str.equals("D")){
+                if(!stk.isEmpty()){
+                    stk.push(stk.peek()*2);
+                }
+            }
+            if(str.equals("+")){
+                if(!stk.isEmpty()){
+                    int first_peek = stk.peek();
+                    int poped = stk.pop();
+                    int sec_peek = 0;
+                    if(!stk.isEmpty())  {
+                        sec_peek = stk.peek();
+                    }
+                    stk.push(poped);
+                    stk.push(first_peek+sec_peek);
+                }
+                
+            }
+        }
+        int total = 0;
+        for(int st: stk){
+            total += st;
+        }
+        return total;
+    }
+}
