c1cd4df062d556d92cabd57383c416c18f0b0dd5
Abhay936 <sa5393359@gmail.com>
2026-08-13 01:15:48 +0530

    Time: 2 ms (93.91%), Space: 72 MB (41.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,49 +1,20 @@
+import java.util.*;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // int n = nums.length;
-        // HashMap<Integer,Integer> map = new HashMap<>();
-        // map.put(0,1);
-        // int ans[] = new int[n];
-        // int sum = 1;
-        // for(int i=0;i<n;i++){
-        //     sum*=nums[i];
-        //     if(map.containsKey(sum)){
-        //         ans[i] = map.get(sum);
-        //     }
-        //     map.put(sum,map.getOrDefault(sum,0)+1);
-        // }
-        // return ans;
-
         int n = nums.length;
         int ans[] = new int[n];
-        int zero_Count = 0;
-        int prod = 1;
+        Arrays.fill(ans,1);
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zero_Count += 1;
-            }
-            else{
-                prod*=nums[i];
-            }
+        int prefix = 1;
+        for(int i=0;i<n;i++){     // prefix product
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
-        if(zero_Count==0){
-            for(int i=0;i<n;i++){
-                ans[i]=(int)prod/nums[i];
-            }
-        }else if(zero_Count==1){
-            for(int i=0;i<n;i++){
-                if(nums[i]!=0){
-                    ans[i] = 0;
-                    continue;
-                }
-                ans[i] = prod;
-            }
-        }
-        else{
-            for(int i=0;i<n;i++){
-                ans[i] = 0;
-            }
+
+        int suffix = 1;
+        for(int i=n-1;i>=0;i--){   //suffix product
+            ans[i] *= suffix;
+            suffix *= nums[i]; 
         }
         return ans;
     }
