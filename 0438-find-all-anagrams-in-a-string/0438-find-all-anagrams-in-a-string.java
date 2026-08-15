17a2d94b615017cce27074dce254043a71ae7742
Abhay936 <sa5393359@gmail.com>
2026-08-15 15:19:13 +0530

    Time: 432 ms (9.03%), Space: 47.6 MB (21%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,29 +2,34 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         int n = s.length();
         int m = p.length();
-        
-        int low = 0;
-        int high = 0;
-        int window_size = m;
-        List<Integer> list = new ArrayList<>();
-
-        if (m > n) {
-            return list;
+        if(m>n){
+            return new ArrayList<>();
         }
-        char[] pArr = p.toCharArray();
-        Arrays.sort(pArr);
-        while(high<n) {
-            if(high-low+1==window_size){     
-                String str = s.substring(low,window_size+low);
-                char arr[] = str.toCharArray();
-                Arrays.sort(arr);
-                if(Arrays.equals(arr,pArr)){
-                    list.add(low);
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        // HashMap<String,Integer> map = new HashMap<>();
+
+        int low = 0,high = 0;
+        int freq[] = new int[26];
+        for(int i=0;i<m;i++){
+            char ch = p.charAt(i);
+            freq[ch-'a']++;
+        }
+        while(high<n){
+            if(high-low+1==m){
+                String str = s.substring(low,low+m);
+                int freq_str[] = new int[26];
+                for(int i=0;i<str.length();i++){
+                    char ch = str.charAt(i);
+                    freq_str[ch-'a']++;
+                }
+                if(Arrays.equals(freq,freq_str)){
+                    arr.add(low);
                 }
                 low++;
             }
             high++;
-        } 
-        return list;
+        }
+        return arr;
     }
 }
