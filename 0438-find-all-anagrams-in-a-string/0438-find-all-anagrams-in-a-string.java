0f55eafbd5b9004226fd8ce28d8200eb147e298e
Abhay936 <sa5393359@gmail.com>
2026-08-15 15:28:08 +0530

    Time: 10 ms (80.92%), Space: 46.6 MB (92.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -7,7 +7,7 @@ class Solution {
         }
 
         ArrayList<Integer> arr = new ArrayList<>();
-        // HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer> map = new HashMap<>();
 
         int low = 0,high = 0;
         int freq[] = new int[26];
@@ -15,18 +15,18 @@ class Solution {
             char ch = p.charAt(i);
             freq[ch-'a']++;
         }
+        int window_freq[] = new int[26];
+
         while(high<n){
+            window_freq[s.charAt(high)-'a']++;
+            if(high-low+1>m){
+                window_freq[s.charAt(low)-'a']--;
+                low++;
+            }
             if(high-low+1==m){
-                String str = s.substring(low,low+m);
-                int freq_str[] = new int[26];
-                for(int i=0;i<str.length();i++){
-                    char ch = str.charAt(i);
-                    freq_str[ch-'a']++;
-                }
-                if(Arrays.equals(freq,freq_str)){
+                if(Arrays.equals(freq,window_freq)){
                     arr.add(low);
                 }
-                low++;
             }
             high++;
         }
