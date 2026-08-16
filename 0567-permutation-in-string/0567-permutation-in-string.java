ac457ca33337eb3d7238969aa2efc2383cc0e9a2
Abhay936 <sa5393359@gmail.com>
2026-08-16 17:25:48 +0530

    Time: 5 ms (97.51%), Space: 43.9 MB (73.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
+        //optimal 
+
         int n = s1.length();
         int m = s2.length();
         if(m<n) return false;
@@ -11,21 +13,21 @@ class Solution {
         for(int i=0;i<n;i++){
             freq[s1.charAt(i)-'a']++;
         }
-
+        int wind_freq[] = new int[26];
         while(high<m){
+            wind_freq[s2.charAt(high)-'a']++;
+            if(high-low+1>wind){
+                wind_freq[s2.charAt(low)-'a']--;
+                low++;
+            }
             if(high-low+1==wind){
-                String str = s2.substring(low,low+wind);
-                int freq_str[] = new int[26];
-                for(int i=0;i<str.length();i++){
-                    freq_str[str.charAt(i)-'a']++;
-                }
-                if(Arrays.equals(freq_str,freq)){
+                if(Arrays.equals(wind_freq,freq)){
                     return true;
                 }
-                low++;
             }
             high++;
         }
+        
         return false;
     }
 }
