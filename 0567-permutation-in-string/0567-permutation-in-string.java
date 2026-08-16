2bfb2ce294840c87723bb7dcdb10fbf686a38d29
Abhay936 <sa5393359@gmail.com>
2026-08-16 17:11:29 +0530

    Time: 77 ms (13.17%), Space: 46.9 MB (17.84%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        //sorting method
-     int n = s1.length();
-     int m = s2.length();
-     if(m<n) return false;
-     char ch1[] = s1.toCharArray();
-     Arrays.sort(ch1);   
-     int low = 0,high = 0;
-     int wind = n;
-     while(high<m){
-        if(high-low+1==wind){
-            String str = s2.substring(low,low+wind);
-            char ch2[] = str.toCharArray();
-            Arrays.sort(ch2);
-            if(Arrays.equals(ch1,ch2)){
-                return true;
-            }
-            low++;
+
+        int n = s1.length();
+        int m = s2.length();
+        if(m<n) return false;
+
+        int low = 0,high = 0;
+        int wind = n;
+        int freq[] = new int[26];
+        for(int i=0;i<n;i++){
+            freq[s1.charAt(i)-'a']++;
         }
-        high++;
-     }
-     return false;
+
+        while(high<m){
+            if(high-low+1==wind){
+                String str = s2.substring(low,low+wind);
+                int freq_str[] = new int[26];
+                for(int i=0;i<str.length();i++){
+                    freq_str[str.charAt(i)-'a']++;
+                }
+                if(Arrays.equals(freq_str,freq)){
+                    return true;
+                }
+                low++;
+            }
+            high++;
+        }
+        return false;
     }
 }
