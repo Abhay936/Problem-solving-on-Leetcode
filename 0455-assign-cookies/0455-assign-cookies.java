bfea26cc9285c5d6f86735a6330b6eb4ee6cb4d7
Abhay936 <sa5393359@gmail.com>
2026-09-12 00:38:04 +0530

    Time: 15 ms (60.01%), Space: 51.3 MB (74.33%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int i=0,j=0;
+        int ans = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                ans++;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+}
